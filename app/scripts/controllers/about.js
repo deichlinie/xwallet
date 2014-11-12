497003384ac7d782fc65035e6397a6a01c03292e
rockhead <smahnke@mail.com>
2014-11-12 23:15:10 +0000

    intial yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xwalletApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xwalletApp
+ */
+angular.module('xwalletApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xwalletApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xwalletApp
+ */
+angular.module('xwalletApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
