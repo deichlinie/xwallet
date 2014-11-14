51aa807b306d63fc3042c60b494ec9a52b2e6046
rockhead <smahnke@mail.com>
2014-11-14 10:29:17 +0000

    osgi style self registration sample

---
+++ app/scripts/controllers/coinregistrycontrol.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xwalletApp.controller:CoinRegistryController
+ * @description
+ * # CoinRegistryController
+ * Controller of the xwalletApp
+ */
+angular.module('xwalletApp')
+  .controller('CoinRegistryController', ['$scope', 'CoinRegistryService', function ($scope, coinRegistry) {
+    $scope.registeredCoins = coinRegistry.getRegisteredCoins();
+  }]);
+
+
