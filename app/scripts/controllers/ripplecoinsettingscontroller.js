2883d7ef40949abb2f0b1258cef26237e5e78b69
rockhead <smahnke@mail.com>
2014-11-14 18:52:53 +0000

    included coin view

---
+++ app/scripts/controllers/ripplecoinsettingscontroller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xwalletApp.controller:RipplecoinsettingscontrollerCtrl
+ * @description
+ * # RipplecoinsettingscontrollerCtrl
+ * Controller of the xwalletApp
+ */
+angular.module('xwalletApp')
+  .controller('RippleCoinSettingsController', function ($scope) {
+    console.log('RippleCoinSettingsController');
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ test/spec/controllers/ripplecoinsettingscontroller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: RipplecoinsettingscontrollerCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('xwalletApp'));
+
+  var RipplecoinsettingscontrollerCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    RipplecoinsettingscontrollerCtrl = $controller('RipplecoinsettingscontrollerCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
